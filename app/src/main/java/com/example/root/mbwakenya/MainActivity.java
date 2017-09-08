198ff2bec7a8a5998076bc9ac3620eccfe4bdc2a
derrickwaweru <derrickwaweru38@gmail.com>
2017-09-08 14:44:43 +0300

    added a button for find dogs

---
+++ app/src/main/java/com/example/root/mbwakenya/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.root.mbwakenya;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindDogsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        mFindDogsButton = (Button) findViewById(R.id.findDogsButton);
+        mFindDogsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, DogsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
