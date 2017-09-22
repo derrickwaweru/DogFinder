05ba0e3bdd912f114ef92a8c5fc5a3b9c13e99a1
derrickwaweru <derrickwaweru38@gmail.com>
2017-09-22 12:22:45 +0300

    changed the photo

--- app/src/main/java/com/example/root/mbwakenya/MainActivity.java
+++ app/src/main/java/com/example/root/mbwakenya/MainActivity.java
@@ -1,32 +1,39 @@
 package com.example.root.mbwakenya;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindDogsButton;
-    private EditText mLocationEditText;
+    @Bind(R.id.findDogsButton) Button mFindDogsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        ButterKnife.bind(this);
+        Typeface gloriaFont = Typeface.createFromAsset(getAssets(), "fonts/GloriaHallelujah.ttf");
+        mAppNameTextView.setTypeface(gloriaFont);
 
-        mFindDogsButton = (Button) findViewById(R.id.findDogsButton);
         mFindDogsButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, DogsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
